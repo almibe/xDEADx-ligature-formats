4a2be226a445e54c349cd7575a2bc2777f128d6f
Alex Michael Berry <berry3@indiana.edu>
2019-07-05 17:03:41 -0400

    rewriting NTriplesSpec

--- src/test/kotlin/org/almibe/ligature/formats/Common.kt
+++ src/test/kotlin/org/almibe/ligature/formats/Common.kt
@@ -12,10 +12,12 @@ import java.io.Reader
 val stringIRI = IRI("http://www.w3.org/2001/XMLSchema#string")
 val spiderMan = IRI ("http://example.org/#spiderman")
 val greenGoblin = IRI ("http://example.org/#green-goblin")
+val blackCat = IRI("")
 val enemyOf = IRI ("http://www.perceive.net/schemas/relationship/enemyOf")
 val thatSeventiesShow = IRI ("http://example.org/show/218")
 val helium = IRI ("http://en.wikipedia.org/wiki/Helium")
 val label = IRI ("http://www.w3.org/2000/01/rdf-schema#label")
+val localName = IRI ("http://example.org/show/localName")
 
 fun readText(resourcePath: String): Reader {
     return InputStreamReader(NTriplesSpec::class.java.getResourceAsStream(resourcePath))
