0ef92fbcdf51349828211148e8b4e1615ca1ba9e
berry3 <berry3@indiana.edu>
2020-12-07 22:15:39 -0500

    filling in NTriplesLexer.read, adding stubs for impl and tests, #6

--- src/main/scala/dev/ligature/formats/ntriples/NTriplesLexer.scala
+++ src/main/scala/dev/ligature/formats/ntriples/NTriplesLexer.scala
@@ -4,47 +4,59 @@
 
 package dev.ligature.formats.ntriples
 
-import cats.effect.IO
-import dev.ligature.{ Statement, BlankNode, Literal, Subject, Object }
 import dev.ligature.formats.common.Walker
-import dev.ligature.iris.IRI
 
 enum NTriplesToken { //TODO probably add parameters to this type to track line and space number
   case IRI(value: String)
   case BlankNodeLabel(value: String)
   case Literal(value: String)
-  case TypeSymbol
+  case TypeSymbol()
   case LangTag(value: String)
-  case EndOfStatement
+  case EndOfStatement()
+  case EndOfLine()
 }
 
 object NTriplesLexer {
+  def read(in: String): Iterator[NTriplesToken] = read(in.iterator)
+
   def read(in: Iterator[Char]): Iterator[NTriplesToken] = {
     val walker = Walker(in)
-    // if (walker.hasNext) {
-    //   val next = walker.next
-    //   next match {
-    //     case '#' => Iterator.empty
-    //     case '<' => Iterator(statement(walker))
-    //     case _        => throw RuntimeException(s"Error on line: $in")
-    //   }
-    // } else {
-    //   Iterator.empty
-    // }
-    ???
+    val res = scala.collection.mutable.ListBuffer[NTriplesToken]()
+
+    while (walker.hasNext) {
+      val char = walker.next
+      val token = char match {
+        case '<'  => iri(walker)
+        case '^'  => typeSymbol(walker)
+        case '@'  => langTag(walker)
+        case '"'  => literal(walker)
+        case '_'  => blankNodeLabel(walker)
+        case '.'  => endOfStatement(walker)
+        case '\n' => endOfLine(walker)
+        case _    => throw RuntimeException(s"Error") //TODO include line + space info
+      }
+      res += token
+    }
+
+    res.iterator
   }
 
-  def iri(walker: Walker): IRI = {
+  private def iri(walker: Walker): NTriplesToken.IRI = {
     val sb = StringBuilder()
     while (walker.hasNext) {
       val next = walker.next
       next match {
-        case '>' => return IRI(sb.toString).getOrElse(throw RuntimeException(s"Couldn't make IRI from ${sb.toString}"))
-        case _        => sb.append(next)
+        case '>' => return NTriplesToken.IRI(sb.toString)
+        case _   => sb.append(next)
       }
     }
     ??? //TODO: probably throw parser exception
   }
-  def blankNode(walker: Walker): BlankNode = ???
-  def literal(walker: Walker): Literal = ???
+
+  private def typeSymbol(walker: Walker): NTriplesToken.TypeSymbol = ???
+  private def langTag(walker: Walker): NTriplesToken.LangTag = ???
+  private def literal(walker: Walker): NTriplesToken.Literal = ???
+  private def blankNodeLabel(walker: Walker): NTriplesToken.BlankNodeLabel = ???
+  private def endOfStatement(walker: Walker): NTriplesToken.EndOfStatement = ???
+  private def endOfLine(walker: Walker): NTriplesToken.EndOfLine = ???
 }
--- src/test/scala/dev/ligature/formats/ntriples/NTriplesLexerSuite.scala
+++ src/test/scala/dev/ligature/formats/ntriples/NTriplesLexerSuite.scala
@@ -6,13 +6,42 @@ package dev.ligature.formats.ntriples
 
 import dev.ligature.Statement
 import dev.ligature.formats.Common._
+import dev.ligature.formats.ntriples._
 import munit.FunSuite
 
 class NTriplesLexerSuite extends FunSuite {
-  test("support basic IRI triple") {
-    val result = NTriples.parseNTriples(readText("ntriples/01-basicTriple.nt")).toSet
-    assert(result.contains(Statement(spiderMan, enemyOf, greenGoblin)))
-    assertEquals(result.size, 1)
+  private def readSingleToken(in: String): NTriplesToken = {
+    val res = NTriplesLexer.read("\"Hello, World\"").toList
+    assertEquals(res.size, 1)
+    res.head
+  }
+
+  test("support basic tokens") {
+    assertEquals(readSingleToken("\"Hello, World\""), NTriplesToken.Literal("Hello, World"))
+    assertEquals(readSingleToken("\"5\""), NTriplesToken.Literal("5"))
+    assertEquals(readSingleToken("<urn:isbn:978-1-93435-645-6>"), NTriplesToken.IRI("urn:isbn:978-1-93435-645-6"))
+    assertEquals(readSingleToken("<https://github.com/scala/scala>"), NTriplesToken.IRI("https://github.com/scala/scala"))
+    assertEquals(readSingleToken("^^"), NTriplesToken.TypeSymbol())
+    assertEquals(readSingleToken("."), NTriplesToken.EndOfStatement())
+    assertEquals(readSingleToken("@jp"), NTriplesToken.LangTag("jp"))
+    assertEquals(readSingleToken("_:bn"), NTriplesToken.BlankNodeLabel("bn"))
+    assertEquals(readSingleToken("\n"), NTriplesToken.EndOfLine())
+  }
+
+  test("handle write space with single token") {
+    ???
+  }
+
+  test("handle comments with single token") {
+    ???
+  }
+
+  test("read multiple tokens") {
+    ???
+  }
+
+  test("handle new lines") {
+    ???
   }
 
   // test("support multiple IRI triples") {
